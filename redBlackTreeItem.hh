06e17279654e0af26ea6a0f20d482bee2e1567a3
neobta <neobta@gmail.com>
2015-06-27 16:01:43 -0500

    Red Black Trees no sentinel

--- redBlackTreeItem.hh
+++ redBlackTreeItem.hh
@@ -5,49 +5,51 @@
 #ifndef REDBLACKTREESDEMO_REDBLACKTREEITEM_H
 #define REDBLACKTREESDEMO_REDBLACKTREEITEM_H
 
+#include <memory>
+
 namespace redBlackTrees {
 
-    enum red_black_tree_color {BLACK, RED};
+  enum red_black_tree_color {BLACK, RED};
 
-    class redBlackTreeItem {
-    private:
+  class redBlackTreeItem {
+  private:
 
-        red_black_tree_color color;
-        int value;
-        redBlackTreeItem *left;
-        redBlackTreeItem *right;
-        redBlackTreeItem *parent;
+    red_black_tree_color color;
+    int value;
+    redBlackTreeItem *left;
+    redBlackTreeItem *right;
+    redBlackTreeItem *parent;
 
-        void insert_fix_up(redBlackTreeItem &newItem);
-
-
-    public:
+    void insert_fix_up(redBlackTreeItem &newItem);
 
 
-        redBlackTreeItem(int i);
-
-        void assign_left(redBlackTreeItem &l);
-
-        void assign_right(redBlackTreeItem &r);
+  public:
 
 
-        redBlackTreeItem* left_rotate();
-        redBlackTreeItem* right_rotate();
-        redBlackTreeItem* insert(redBlackTreeItem &newItem);
+    redBlackTreeItem(int i);
 
-        int get_value();
+    void assign_left(redBlackTreeItem &l);
+
+    void assign_right(redBlackTreeItem &r);
 
 
-        void print_in_order();
+    redBlackTreeItem* left_rotate();
+    redBlackTreeItem* right_rotate();
+    redBlackTreeItem* insert(redBlackTreeItem &newItem);
 
-        void print_pre_order();
+    int get_value();
 
-        void print_post_order();
 
-        void assign_node_as_red();
-        void assing_node_as_black();
-        redBlackTreeItem* get_root();
-    };
+    void print_in_order();
+
+    void print_pre_order();
+
+    void print_post_order();
+
+    void assign_node_as_red();
+    void assing_node_as_black();
+    redBlackTreeItem* get_root();
+  };
 }
 
 #endif //REDBLACKTREESDEMO_REDBLACKTREEITEM_H
